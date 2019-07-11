d6417b0efc9862daeaedbd40d80476b699009c34
Nelson <nelsonsprague84@gmail.com>
2019-07-10 20:38:21 -0600

    done for the night bringing the #each burger question in tomorrow

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,57 +4,31 @@ var router = express.Router();
 
 var burger = require("../models/burger")
 
-router.get("/", function(req, res){
-    burger.all(function(data){
-        var hbsObject = {all: function(tableInput, cb) {
-            var queryString = "SELECT * FROM " + tableInput + ";";
-            connection.query(queryString, function(err, result) {
-              if (err) {
-                throw err;
-              }
-              cb(result);
-            });
-          },
-          create: function(table, cols, vals, cb) {
-            var queryString = "INSERT INTO " + table;
-        
-            queryString += " (";
-            queryString += cols.toString();
-            queryString += ") ";
-            queryString += "VALUES (";
-            queryString += printQuestionMarks(vals.length);
-            queryString += ") ";
-        
-            console.log(queryString);
-        
-            connection.query(queryString, vals, function(err, result) {
-              if (err) {
-                throw err;
-              }
-        
-              cb(result);
-            });
-          },
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
             burger: data
         };
         console.log(hbsObject)
         res.render("index", hbsObject);
-    })
-})
-router.post("/api/burgers", function(req, res){
-    burger.create([
-        "name", "devoured"
-    ],[
-        req.body.name, req.body.devoured
-    ], function(result){
-        res.json({ id: result.insertId})
     });
 });
-router.delete("/api/burgers/:id", function(req,res){
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function (result) {
+        res.json({
+            id: result.insertId
+        })
+    });
+});
+router.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
-    burger.delete(condition, function(result){
-        if (result.affectedRows == 0){
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
             return res.status(404).end();
         }
     });
